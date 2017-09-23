a651543723fa0e55e71df640e4dd7faef334586f
Timothy Soehnlin <tim@eaiti.com>
2017-09-23 19:26:44 -0400

    Working on extension

--- src/extension.ts
+++ src/extension.ts
@@ -7,23 +7,62 @@ import * as vscode from 'vscode';
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "encore2-test-plugin" is now active!');
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "encore2-test-plugin" is now active!');
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
+  let timeout = null;
+  let activeEditor = vscode.window.activeTextEditor;
+  let dec = vscode.window.createTextEditorDecorationType({
+    isWholeLine: true,
+    overviewRulerColor: 'rgba(255, 0, 0, .6)',
+    overviewRulerLane: vscode.OverviewRulerLane.Right,
+    light: {
+      // this color will be used in light color themes
+      after: {
+        color: 'darkgrey'
+      }
+    },
+    dark: {
+      // this color will be used in dark color themes
+      after: {
+        color: 'lightgray'
+      }
+    },
+    after: {
+      textDecoration: 'font-style: italics;',
+      contentText: ' After the row'
+    }
+  });
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
+  function updateDecorations() {
+    if (!activeEditor) {
+      return;
+    }
+    activeEditor.setDecorations(dec, [new vscode.Range(activeEditor.selection.start.line, 0, activeEditor.selection.start.line, 100000000000)]);
+  }
 
-    context.subscriptions.push(disposable);
-}
+  function triggerUpdateDecorations() {
+    if (timeout) {
+      clearTimeout(timeout);
+    }
+    timeout = setTimeout(updateDecorations, 500);
+  }
 
-// this method is called when your extension is deactivated
-export function deactivate() {
+  if (activeEditor) {
+    triggerUpdateDecorations();
+  }
+
+  vscode.window.onDidChangeActiveTextEditor(editor => {
+    activeEditor = editor;
+    if (editor) {
+      triggerUpdateDecorations();
+    }
+  }, null, context.subscriptions);
+
+  vscode.workspace.onDidChangeTextDocument(event => {
+    if (activeEditor && event.document === activeEditor.document) {
+      triggerUpdateDecorations();
+    }
+  }, null, context.subscriptions);
 }
