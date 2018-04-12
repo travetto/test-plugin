3f92022bdf155a281724a648c0362b0327d3e17b
Timothy Soehnlin <tim@eaiti.com>
2018-04-12 09:13:05 -0400

    Working like a charm

--- src/extension.ts
+++ src/extension.ts
@@ -7,31 +7,31 @@ import { Decorations } from './decoration';
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
   Decorations.context = context;
+  try {
+    const runner = new TestRunner();
 
-  const runner = new TestRunner();
-  const isEditor = (o: any): o is vscode.TextEditor => o && 'document' in o;
+    function onUpdate(target?: vscode.TextDocument) {
 
-  function onUpdate(target?: vscode.TextEditor | vscode.TextDocument) {
+      const editor = vscode.window.activeTextEditor;
 
-    const editor = vscode.window.activeTextEditor;
-
-    if (!isEditor(target)) {
-      if (!editor || editor.document !== target) {
+      if (!editor || (target && editor.document !== target)) {
         return;
       }
-    }
 
-    if (editor.document && /@Test\(/.test(editor.document.getText() || '')) {
-      runner.run(editor, isEditor(target)).catch(e => console.error(e));
-    }
-  };
+      if (editor.document && /@Test\(/.test(editor.document.getText() || '')) {
+        runner.run(editor, !target).catch(e => console.error(e));
+      }
+    };
 
-  vscode.window.onDidChangeActiveTextEditor(onUpdate, null, context.subscriptions);
-  vscode.workspace.onDidSaveTextDocument(onUpdate, null, context.subscriptions);
+    vscode.window.onDidChangeActiveTextEditor(x => onUpdate(), null, context.subscriptions);
+    vscode.workspace.onDidSaveTextDocument(onUpdate, null, context.subscriptions);
 
-  onUpdate();
+    onUpdate();
 
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "travetto-test-plugin" is now active!', `${__dirname}/success.png`);
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // This line of code will only be executed once when your extension is activated
+    console.log('Congratulations, your extension "travetto-test-plugin" is now active!', `${__dirname}/success.png`);
+  } catch (e) {
+    console.error('WOAH', e);
+  }
 }
